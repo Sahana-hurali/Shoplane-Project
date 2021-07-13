741cf3259af805889d44a0fd04c3cafb40f36d22
Sahana-hurali <sahanahurali32@gmail.com>
2021-07-13 14:09:28 +0530

    API file uploaded

---
+++ src/utils/apis/index.js
@@ -0,0 +1,26 @@
+const corseUnBlock = "https://cors-anywhere.herokuapp.com/";
+
+export const homeProductsAPI =
+  "https://607e95f802a23c0017e8ba2f.mockapi.io/habib-shoplane";
+
+export const getProduct =
+'https://5d76bf96515d1a0014085cf9.mockapi.io/product/';
+
+export const sliderAPI = [
+  {
+    "img": "https://shoplane.netlify.app/img/img1.png",
+    "alt" : "Image 1"
+  },
+  {
+    "img": "https://shoplane.netlify.app/img/img2.png",
+    "alt" : "Image 2"
+  },
+  {
+    "img": "https://shoplane.netlify.app/img/img3.png",
+    "alt" : "Image 3"
+  },
+  {
+    "img": "https://shoplane.netlify.app/img/img4.png",
+    "alt" : "Image 4"
+  },
+]
