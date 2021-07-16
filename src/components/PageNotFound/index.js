b0c108a00eab25ed13493150799489a7efa917bc
Sahana-hurali <sahanahurali32@gmail.com>
2021-07-16 12:52:31 +0530

    page not found uploaded

---
+++ src/components/PageNotFound/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import PageNotFoundImg from '../../Images/PageNotFoundImg.jpg';
+
+const PageNotFound = () => {
+    return ( <div className="m-4 d-flex justify-content-center">
+        <img src={'https://i2.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1'} alt="404 Page Not Found" style={{width:"50vw"}}/>
+    </div> );
+}
+ 
+export default PageNotFound;
